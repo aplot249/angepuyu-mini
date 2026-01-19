b383e2f8973122efa4a6d3914a942085fb2998a8
Three_laptop <a17868810687@gmail.com>
2026-01-20 00:36:42 +0300

    ijij

--- requests/index.js
+++ requests/index.js
@@ -3,7 +3,7 @@ import {encrypt,decrypt} from '../utils/encryption.js';
 
 // const baseHOST = 'http://192.168.0.67:8000' //公司
 const baseHOST = 'https://siyu.jsxinlingdi.com'
-// const baseHOST =  'http://192.168.1.181:8000'  //住宿
+// const baseHOST =  'http://192.168.1.181:8000' //住宿
 // const baseHOST =  'http://127.0.0.1:8000'
 
 // const isJiami = false
